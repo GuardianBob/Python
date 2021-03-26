b7c6036858ca6c7bf25a860d8a3acfb2a808b303
GuardianBob <78508863+GuardianBob@users.noreply.github.com>
2021-03-26 10:27:42 -0700

    Fixed Django db assnmt

--- Django/django_orm/single_model_orm/users_app/urls.py
+++ Django/django_orm/single_model_orm/users_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index),
     path('submit', views.submit),
+    path('delete/<int:uid>', views.delete),
 ]
--- Django/django_orm/single_model_orm/users_app/views.py
+++ Django/django_orm/single_model_orm/users_app/views.py
@@ -1,17 +1,46 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import User
 
-def index(request):
+def index(request):    
+    all_users = User.objects.all()
+    # all_fields = User._meta.get_fields()    # Get field names
+    # obj2 = all_fields[1].name   #get actual name of field
+    # obj1 = all_users[0]         
+    # name = getattr(obj1, obj2)      #Gets the value of the second field for the first user
+    # print(name, len(all_fields))
+
+    users = {}
+    n = 0
+    for i in all_users:
+        users[str(n)] = {
+            'id':all_users[n].id,
+            'name':all_users[n].first_name + " " + all_users[n].last_name,
+            'email':all_users[n].email_address,
+            'age':all_users[n].age
+        }
+        n +=1
+    # print(users)
+
+    # Iterate through nested dictionary
+    # for p_k, p_v in users.items():  
+    #     for value in p_v:
+    #         print(p_v[value])
     context = {
-        "all_users": User.objects.all()
+        # "all_users": User.objects.all()
+        "all_users": users
     }
     return render(request, 'index.html', context)
+
  
-def new_user(requests):
-    nUsr = User.Objects.create()
-    nUsr.first_name = request.sessions.POST('fName')
-    nUsr.last_name = request.sessions.POST('lName')
-    nUsr.email_address = request.sessions.POST('email')
-    nUsr.age = request.sessions.POST('age')
-    nUsr.save()
+def submit(request):    
+    fName = request.POST['fName']
+    lName = request.POST['lName']
+    email = request.POST['email']
+    age = request.POST['age']
+    User.objects.create(first_name=fName, last_name=lName, email_address=email, age=age)
+    return redirect('/')
+
+def delete(request, uid):
+    del1=User.objects.get(id=uid)
+    del1.delete()
     return redirect('/')
